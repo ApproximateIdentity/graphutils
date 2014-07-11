ec3e0eee02c8cdbfec8f0115d3a375412b5fe78e
Thomas Nyberg <tomnyberg@gmail.com>
2014-07-11 14:17:43 -0400

    Refractor and clean code

--- examples/pageRankCut.py
+++ examples/pageRankCut.py
@@ -28,89 +28,62 @@ def getVertInList(vertOutList):
     return vertInList
 
 
-def init_distribution(s, dim):
+def init_distribution(pnode, graph):
     """
-    returns the initial PageRank distribution
-    #(1 at s, 0 everywhere else)
+    Initializes a distribution with all weight at node pnode.
     """
-    q = []
-    for _ in range(dim):
-        q.append(0.0)
-    q[s] = 1.0
-    return q
+    dist = {}
+    for node, _ in graph:
+        dist[node] = 0.0
+    dist[pnode] = 1.0
+    return dist
 
 
-def computeRank(A, dim):
-    """
-    returns a list containing the number of outbound edges
-    for each vertex
-    """
-    rank = [len(a) for a in A]
-    return rank
-
-
-def new_distribution(dist_old, B, dim, alpha, rank, s):
+def new_distribution(distold, graph, alpha, pnode):
     """
     #returns the distribution after another iteration of
     #the PageRank algorithm
     """
-    newdist = []
-    for i in range(dim):
-        newdist.append(0.0)
-    newdist[s] = 1 - alpha
-    for i in range(dim):
-        b = B[i]
-        newdist[i] += .5 * alpha * dist_old[i]
-        for x in b:
-            x = int(x)
-            newdist[i] += .5 * alpha * (dist_old[x] / rank[x])
+    newdist = {}
+    for node, _ in graph:
+        newdist[node] = 0.0
+    newdist[pnode] = 1 - alpha
+    for node, targets in graph:
+        newdist[node] += .5 * alpha * distold[node]
+        for target in targets:
+            newdist[target] += .5 * alpha * (distold[node] / len(targets))
     return newdist
 
 
-def distance(p, q):
+def distance(dist1, dist2):
     """
     #returns the L1 distance between n-1st and nth
     #distributions
     """
-    dist = 0.
-    for i, j in zip(p, q):
-        dist += abs(i - j)
+    dist = 0.0
+    for key in dist1.iterkeys():
+        dist += abs(dist1[key] - dist2[key])
     return dist
 
 
-def getVector(dim, A, B, p):
+def getVector(graph, dist):
     """
     # returns a list of pairs (a, b) with a a vertex
     # and b it's pageRank value
     """
-    i = 0
     v = []
-    for i in range(dim):
-        x = []
-        x.append(i)
-        #Extremely weird way to do degree
-        x.append(p[i]/(.5*(len(A[i]) + len(B[i]))))
-        v.append(x)
+    for node, targets in graph:
+        v.append([node, dist[node]/len(targets)])
     return v
 
 
-def sortIndices(v):
-    """
-    #sorts a list of pairs(see getVector) from least to greatest
-    #to compute the pageRank ordering of the vertices
-    """
-    I = sorted(v, key=lambda a: a[1])
-    I = [pair[0] for pair in I]
-    return I
-
-
-def getTotalVol(A, B):
+def getTotalVol(graph):
     """
     Return total volume of a graph.
     """
-    tVol = 0.
-    for a, b in zip(A, B):
-        tVol += .5*(len(a) + len(b))
+    tVol = 0.0
+    for _, targets in graph:
+        tVol += len(targets)
     return tVol
 
 
@@ -154,9 +127,9 @@ if __name__ == '__main__':
                         help='Alpha parameter.',
                         type=float,
                         default=0.5)
-    parser.add_argument('-p', '--personalizednode', dest='personalizednode',
+    parser.add_argument('-p', '--personalizednode', dest='pnode',
                         help='Alpha parameter.',
-                        type=int,
+                        type=str,
                         default=0)
     args = parser.parse_args()
 
@@ -173,42 +146,34 @@ if __name__ == '__main__':
     alpha = args.alpha
     assert(alpha >= 0.0 and alpha <= 1.0)
 
-    personalizednode = args.personalizednode
-    assert(personalizednode >=0 and personalizednode < dim)
+    pnode = args.pnode
 
-    vertOutList = getVertOutList(graph)
+    distold = init_distribution(pnode, graph)
 
-    vertInList = getVertInList(vertOutList)
+    distnew = new_distribution(distold, graph, alpha, pnode)
 
-    dist_old = init_distribution(personalizednode, dim)
-
-    rank = computeRank(vertOutList, dim)
-
-    dist_new = new_distribution(dist_old, vertInList, dim, alpha, rank,
-                                personalizednode)
-
-    dist = distance(dist_new, dist_old)
     iter = 1
     #iterates the PageRank algorithm until it converges
-    while (dist > .0000001):
-        dist_old = dist_new
-        dist_new = new_distribution(dist_old, vertInList, dim, alpha, rank,
-                                    personalizednode)
-        dist = distance(dist_old, dist_new)
+    while (distance(distnew, distold) > .0000001):
+        distold = distnew
+        distnew = new_distribution(distold, graph, alpha, pnode)
         iter += 1
-    #print "distribution is:"
-    #print p
+
     print "iterations: %d" %iter
-    v = getVector(dim, vertOutList, vertInList, dist_new)
-    S = sortIndices(v)
+    v = getVector(graph, distnew)
+
+    S = [node for node, _ in sorted(v, key=lambda a: a[1])]
+
     #print "S:"
     #print S
-    tVol = getTotalVol(vertOutList, vertInList)
+    tVol = getTotalVol(graph)
     i = 0
     minH = 2
     ind = 0
     #computes CheagerRatio of each S(i), keeping track
     # of smallest one along the way
+    vertOutList = getVertOutList(graph)
+    vertInList = getVertInList(vertOutList)
     while (i < dim-1):
         x = getCheager(S, i, vertOutList, vertInList, tVol)
         if (x > .0001 and x < minH):
